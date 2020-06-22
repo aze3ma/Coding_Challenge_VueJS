99065777ed6995faffc42fe8e548d97753ec29f3
Mahmoud Abdulazim <aze3ma@gmail.com>
2020-06-22 21:08:38 +0200

    fix import name for the App component

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import '@babel/polyfill'
 import Vue from 'vue'
 import './plugins/vuetify'
-import App from './app'
+import App from './App'
 import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
